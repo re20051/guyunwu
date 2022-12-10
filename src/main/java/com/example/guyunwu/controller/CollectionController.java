777e51f833df72b5febc8207721c4f571e5fc476
Uzemiu <1449851534@qq.com>
2022-12-10 21:30:19 +0800

    feat: 每日一句接口

--- src/main/java/com/example/guyunwu/controller/CollectionController.java
+++ src/main/java/com/example/guyunwu/controller/CollectionController.java
@@ -1,25 +1,45 @@
 package com.example.guyunwu.controller;
 
+import com.example.guyunwu.model.entity.DailySentence;
 import com.example.guyunwu.model.response.Result;
+import com.example.guyunwu.repository.DailySentenceRepository;
 import com.example.guyunwu.service.CollectionService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/collection")
+@RequiredArgsConstructor
 public class CollectionController {
 
-    @Autowired
-    private CollectionService collectionService;
+    private final CollectionService collectionService;
+
+    private final DailySentenceRepository dailySentenceRepository;
 
     @ApiOperation("每日一句")
     @GetMapping(value = "/dailySentence")
-    public Result dailySentence() {
+    public Result<List<DailySentence>> dailySentence(Pageable pageable) {
+        PageRequest pageRequest = PageRequest.of(
+                pageable.getPageNumber(),
+                pageable.getPageSize(),
+                Sort.by(Sort.Direction.DESC, "time"));
 
-        return Result.ok();
+        Specification<DailySentence> specification = (root, criteriaQuery, criteriaBuilder) ->
+                criteriaBuilder.lessThanOrEqualTo(root.get("time"), LocalDateTime.now());
+        Page<DailySentence> p = dailySentenceRepository.findAll(specification, pageRequest);
+        return Result.ok("ok", p.toList());
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////
--- src/main/java/com/example/guyunwu/controller/UserController.java
+++ src/main/java/com/example/guyunwu/controller/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
